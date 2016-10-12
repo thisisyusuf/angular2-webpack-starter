9a0d29ef5932911ab21b096c22fd620c0e801dc9
Yusuf Kibaya <yusuf.kibaya@gmail.com>
2016-10-12 13:26:47 +0300

    Added jQuery and associated Typesctipt definitions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,8 @@ import '../../public/css/styles.css';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent {
+	clickJquery(): void{
+		$( "img" ).fadeOut("slow");
+	}
+}
